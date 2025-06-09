84ce422e268e0d6526cce345a57f361c77d3c9d6
baobui13 <23520093@gm.uit.edu.vn>
2025-06-09 17:36:07 +0700

    category fragment
    category list fragment
    category item
    category list adapter

---
+++ app/src/main/java/com/example/happy_wallet_mobile/View/Fragment/CategoryListFragment.java
@@ -0,0 +1,45 @@
+package com.example.happy_wallet_mobile.View.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.happy_wallet_mobile.Model.Category;
+import com.example.happy_wallet_mobile.R;
+import com.example.happy_wallet_mobile.View.Adapter.CategoryListViewAdapter;
+import com.example.happy_wallet_mobile.View.Adapter.NotificationListViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CategoryListFragment extends Fragment {
+
+    FrameLayout flAddCategory;
+    ListView lvCategoryList;
+    List<Category> categoryList;
+    CategoryListViewAdapter categoryListViewAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_category_list, container, false);
+
+        flAddCategory = view.findViewById(R.id.flAddCategory);
+        lvCategoryList = view.findViewById(R.id.lvCategoryList);
+
+        categoryList = new ArrayList<Category>();
+        categoryList.add(new Category("Title 1"));
+        categoryList.add(new Category("Title 2"));
+
+        categoryListViewAdapter = new CategoryListViewAdapter(requireContext(), categoryList);
+        lvCategoryList.setAdapter(categoryListViewAdapter);
+
+        return view;
+    }
+}
