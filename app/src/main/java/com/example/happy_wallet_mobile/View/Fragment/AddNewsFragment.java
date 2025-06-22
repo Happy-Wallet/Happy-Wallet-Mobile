fe2557a9f464870a9002eb999ec66b084d5b38a1
baobui13 <23520093@gm.uit.edu.vn>
2025-06-22 13:43:13 +0700

    //

--- app/src/main/java/com/example/happy_wallet_mobile/View/Fragment/AddNewsFragment.java
+++ app/src/main/java/com/example/happy_wallet_mobile/View/Fragment/AddNewsFragment.java
@@ -2,12 +2,15 @@ package com.example.happy_wallet_mobile.View.Fragment;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +18,8 @@ import com.example.happy_wallet_mobile.R;
 
 public class AddNewsFragment extends Fragment {
 
+    ConstraintLayout clContentLayout;
+    FrameLayout flButtonLayout;
     TextView tvNews, tvPost;
     ImageView ivImage, ivChooseImage, ivAddActivity;
     EditText etDescription;
@@ -25,6 +30,8 @@ public class AddNewsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_news, container, false);
 
+        clContentLayout = view.findViewById(R.id.clContentLayout);
+        flButtonLayout = view.findViewById(R.id.flButtonLayout);
         tvNews = view.findViewById(R.id.tvNews);
         tvPost = view.findViewById(R.id.tvPost);
         ivImage = view.findViewById(R.id.ivImage);
@@ -34,6 +41,12 @@ public class AddNewsFragment extends Fragment {
 
         // default view
         ivImage.setVisibility(View.GONE);
+        ConstraintSet clContentLayoutConstraintSet = new ConstraintSet();
+        clContentLayoutConstraintSet.clone(clContentLayout);
+
+        clContentLayoutConstraintSet.clear(R.id.etDescription, ConstraintSet.TOP);
+        clContentLayoutConstraintSet.connect(R.id.etDescription, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 20);
+        clContentLayoutConstraintSet.applyTo(clContentLayout);
 
         return view;
     }
