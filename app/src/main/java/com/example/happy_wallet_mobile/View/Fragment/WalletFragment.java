472f7cc5ca55592fc2f46a9e35f5c664a8159250
baobui13 <23520093@gm.uit.edu.vn>
2025-06-23 15:07:29 +0700

    clear daily Transaction list in Wallet Fragment

--- app/src/main/java/com/example/happy_wallet_mobile/View/Fragment/WalletFragment.java
+++ app/src/main/java/com/example/happy_wallet_mobile/View/Fragment/WalletFragment.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 public class WalletFragment extends Fragment {
 
-    List<DailyTransactionList> dailyTransactionListItemList;
+    List<DailyTransactionList> dailyTransactionListItemList = new ArrayList<DailyTransactionList>();
     RecyclerView rvTransactions;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,20 +31,6 @@ public class WalletFragment extends Fragment {
 
         rvTransactions = view.findViewById(R.id.rvTransactions);
 
-        dailyTransactionListItemList = new ArrayList<>();
-
-        List<Transaction> day1Transactions = Arrays.asList(
-                new Transaction("Eating", "150", "26/5/2025", "Food", "Bun bo cham nuoc mam siêu ngonnnnnnnn", "expenditure"),
-                new Transaction("Salary", "150", "26/5/2025", "Income", "Cong ty nay tra luong minh", "income"),
-                new Transaction("Saving for Cars", "5", "26/5/2025", "Saving", "sieu xe sieu nhanh sieu manh", "expenditure")
-        );
-        dailyTransactionListItemList.add(new DailyTransactionList("26/5/2025", "-5$", day1Transactions));
-        List<Transaction> day2Transactions = Arrays.asList(
-                new Transaction("Eating", "150", "25/5/2025", "Food", "my goi thoi ma tan 150 nghin", "expenditure"),
-                new Transaction("Eating", "150", "25/5/2025", "Food", "skibidi yeat yeat", "expenditure"),
-                new Transaction("Eating", "150", "25/5/2025", "Food", "Mi cay han quoc sieu ngon sieu cay ba chay bo chet", "expenditure")
-        );
-        dailyTransactionListItemList.add(new DailyTransactionList("25/5/2025", "-450$", day2Transactions));
 
         DailyTransactionsRecyclerViewAdapter adapter = new DailyTransactionsRecyclerViewAdapter(dailyTransactionListItemList);
         rvTransactions.setLayoutManager(new LinearLayoutManager(requireContext()));
